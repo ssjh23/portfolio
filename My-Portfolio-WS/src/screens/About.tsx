952fd4776a3e7f633e0579aa7d544f79d86c00f4
ssjh23 <seansoojh23@gmail.com>
2022-10-15 18:23:06 +0800

    feat: Added About me and Skills

---
+++ My-Portfolio-WS/src/screens/About.tsx
@@ -0,0 +1,8 @@
+
+export default function About () {
+    return (
+        <div className="bg-slate-800 h-screen w-screen col-start-2 col-span-5">
+            <span>About Me</span>
+        </div>
+    )
+}
---
+++ My-Portfolio-WS/src/screens/Skills.tsx
@@ -0,0 +1,7 @@
+export default function Skills () {
+    return (
+        <div className="bg-slate-800 h-screen w-screen col-start-2 col-span-5" id="skills">
+            <span>Skills </span>
+        </div>
+    )
+}
