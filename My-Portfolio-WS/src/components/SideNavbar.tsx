dc95cb53f32310764ea1e47ce24770f5c962793a
ssjh23 <seansoojh23@gmail.com>
2022-10-16 23:37:56 +0800

    fix: Changed fonts of NavBar

--- My-Portfolio-WS/src/components/SideNavbar.tsx
+++ My-Portfolio-WS/src/components/SideNavbar.tsx
@@ -3,7 +3,7 @@ import { HashLink } from "react-router-hash-link"
 
 export default function SideNavbar () {
     return(
-        <div className="h-screen col-start-1 col-span-1 relative top-0">
+        <div className="h-screen col-start-1 col-span-1 sticky top-0 z-40">
             <div className="font-shareTechMono flex flex-col">
                 <HashLink to='#about'>
                     About
