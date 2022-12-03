deb6a2fcb41cbf969084fed72df2075791f6d08e
ssjh23 <seansoojh23@gmail.com>
2022-12-03 20:10:11 +0800

    fix: Realigned Skills elements

--- My-Portfolio-WS/src/App.tsx
+++ My-Portfolio-WS/src/App.tsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 import { BrowserRouter } from 'react-router-dom'
-import { HashLink } from 'react-router-hash-link'
 //Component Import
 import About from './screens/About'
 import Skills from './screens/Skills'
