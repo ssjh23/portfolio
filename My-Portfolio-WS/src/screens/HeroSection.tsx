d265b2bcb8b4148e9e9d636dffdf740b2a5ff6bb
ssjh23 <64569228+ssjh23@users.noreply.github.com>
2022-12-20 01:41:56 +0800

    fix: HeroSection Responsive CSS

--- My-Portfolio-WS/src/screens/HeroSection.tsx
+++ My-Portfolio-WS/src/screens/HeroSection.tsx
@@ -33,9 +33,9 @@ export default function HeroSection ({isRenderOtherPages} :P) {
     return (
         <motion.div 
             ref={ref} 
-            className="flex bg-slate-900 h-screen" 
+            className="flex bg-slate-900 h-screen justify-start" 
             id="Intro">
-            <div className="flex flex-col xl:ml-60 2xl:ml-80 pl-32 pt-40">
+            <div>
                 <HeroSectionSVG isSVGCompleted={isSVGAnimationComplete}/>
                 {isSVGCompleted && (
                     <>
@@ -43,18 +43,19 @@ export default function HeroSection ({isRenderOtherPages} :P) {
                             initial={{opacity:0, x:-100}} 
                             animate={{opacity:1, x:0}} 
                             transition={{delay:0.5, duration:0.8}} 
-                            className="text-xl font-shareTechMono text-left text-teal-400 mt-6">
+                            className="text-xl font-shareTechMono text-center text-teal-400 mt-20 md:text-left md:px-10 lg:px-20 2xl:text-2xl 2xl:px-48">
                                     Hey, Sean here
                         </motion.div>
                         <motion.div 
                         initial={{opacity:0, x:-100}} 
                         animate={{opacity:1, x:0}} 
                         transition={{delay:0.5, duration:0.8}} 
-                        className="text-xl text-left text-slate-400 mt-6 w-1/2">
+                        className="text-base text-center text-slate-400 px-10 mt-6 max-w-screen-xl md:text-left lg:px-20 2xl:text-xl 2xl:px-48">
                                 I am a Computer Science Undergrad in my 3rd year at <a href="https://istd.sutd.edu.sg/" target="_blank" rel="noopener noreferrer" className="text-teal-400 hover:underline underline-offset-1">Singapore University of Technology and Design</a>. 
                                 An aspiring Software Engineer looking, I enjoy making things work with code.
                                 <br></br><br></br>Currently, I am interning at <a href="https://www.voltality.io/" target="_blank" rel="noopener noreferrer" className="text-teal-400 hover:underline underline-offset-1">Voltality</a> as a Frontend Engineer
                         </motion.div>
+                        <div className="md:flex md:px-10 lg:px-20 2xl:px-48">
                         <motion.button 
                             initial={{opacity:0, x:-100}} 
                             animate={{opacity:1, x:0, transition:{delay:0.5, duration:0.8}}}
@@ -67,9 +68,10 @@ export default function HeroSection ({isRenderOtherPages} :P) {
                                 backgroundColor:"#fff"
                             }}
                             onClick={handleRedirect}
-                            className="self-start mt-12 border-solid border-teal-400 border-2 p-4 rounded-lg text-teal-400 font-shareTechMono">
+                            className="border-solid border-teal-400 border-2 p-4 rounded-lg text-teal-400 font-shareTechMono mt-12 md:self-start">
                                 Find out more about me!
                         </motion.button>
+                        </div>
                     </>
                 )}
             </div>
